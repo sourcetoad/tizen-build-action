445a62f7a99f81442715d70af0d556ffca2b33fb
Hacone <mouren1101@outlook.com>
2024-03-10 14:23:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ EOF
 PACKAGE_OUTPUT_PATH="$PROJECT_DIR/output.wgt"
 ERROR_LOG="$GITHUB_WORKSPACE/tizen-studio-data/cli/logs/cli.log"
 
-tizen build-web -- "$PROJECT_DIR" \
+tizen build-web -e .git/ -e .github -- "$PROJECT_DIR" \
     && tizen package -t wgt -s sourcetoad-tizen-public -o "$PACKAGE_OUTPUT_PATH" -- "$PROJECT_DIR/.buildResult"
 
 if [ $? -eq 0 ]; then
