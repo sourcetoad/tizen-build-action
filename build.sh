3ff581d07ed16e9f6a6ec3c01acb391119f7f65c
Hacone <mouren1101@outlook.com>
2024-03-10 15:15:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ echo -n "$3" | base64 -d >"$AUTHOR_KEY"
 AUTHOR_PASSWORD="$4"
 
 tizen security-profiles add -a $AUTHOR_KEY -n sourcetoad-tizen-public -p $AUTHOR_PASSWORD
-tizen cli-config "profiles.path=/home/runner/work/tizen_novel/tizen_novel/tizen-studio-data/profile/profiles.xml"
+tizen cli-config -g "profiles.path=/home/runner/work/tizen_novel/tizen_novel/tizen-studio-data/profile/profiles.xml"
 if [ ! -z $5 ]; then
     CUSTOM_DISTRIBUTOR_CERT="$GITHUB_WORKSPACE/distributor-cert.cer"
     echo -n "$5" | base64 -d >"$CUSTOM_DISTRIBUTOR_CERT"
